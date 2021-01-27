a9e0b11dd6cd6e4cd8faf0d28577d6610442d43e
Jens Vagelpohl <jens@netz.ooo>
2021-01-27 13:23:40 +0100

    - fix PyPI classifiers

--- setup.py
+++ setup.py
@@ -16,12 +16,17 @@ setup(
     # Get more strings from
     # http://pypi.python.org/pypi?:action=list_classifiers
     classifiers=[
-        "Framework :: Plone",
-        "Framework :: Plone :: 4.3",
-        "Framework :: Plone :: 5.0",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 2.7",
-        "Operating System :: OS Independent"
+        'Framework :: Plone',
+        'Framework :: Plone :: 5.2',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: OS Independent'
     ],
     keywords='Plone undelete garbage trash can',
     author='enfold',
